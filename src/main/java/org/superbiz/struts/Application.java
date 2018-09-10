ab494f6ec8b1f5b84e5e7d5fa87b3b9bc162f629
Sranjan <sranjan@pivotal.io>
2018-09-10 15:26:34 +0800

    Struts to Spring

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -10,42 +10,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    /*
-    @Bean
-    public FilterRegistrationBean filterDispatcher() {
-        return buildFilterRegistration(0, new FilterDispatcher());
-    }
-
-    @Bean
-    public FilterRegistrationBean actionContextCleanup() {
-        return buildFilterRegistration(1, new ActionContextCleanUp());
-    }
-
-    @Bean
-    public FilterRegistrationBean sitemeshPageFilter() {
-        return buildFilterRegistration(2, new PageFilter());
-    }
-
-
-    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        //registration.setUrlPatterns(singletonList("/*"));
-
-        registration.setUrlPatterns(asList(
-                "/decorators/layout.jsp",
-//                "/addUser.action",
-                "/addedUser.jsp",
-//                "/findUser.action",
-//                "/findUserForm.action",
-//                "/findUserForm.jsp",
-                "/displayUser.jsp",
-                "/displayUsers.jsp",
-                "/listAllUsers.action"
-        ));
-
-        registration.setOrder(order);
-        return registration;
-    }
-    */
 }
